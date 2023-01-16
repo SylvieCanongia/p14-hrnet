f80971df487e176d97c2876e169ef0962c395fff
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2023-01-16 12:42:20 +0100

    :sparkles: added Routes, Error page component and some scss vars

---
+++ src/components/Routes/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import EmployeeList from "../../pages/EmployeeList/EmployeeList";
+import CreateEmployee from "../../pages/CreateEmployee/CreateEmployee";
+import Error from "../../pages/Error/Error";
+
+const index = () => {
+  return (
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<EmployeeList />} />
+        <Route path="/create-employee" element={<CreateEmployee />} />
+        {/* default route if no other matches */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </React.StrictMode>
+  );
+};
+
+export default index;
---
+++ src/pages/Error/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./error.scss";
+
+const Error = () => {
+  return (
+    <div className='error'>
+      <h1 className="errorTitle">404</h1>
+      <p className="errorText">Whoops ! The page you requested does not exist.</p>
+      <Link to='/' className='errorHomeLink'>Return to the home page</Link>
+    </div>
+  );
+};
+
+export default Error;
